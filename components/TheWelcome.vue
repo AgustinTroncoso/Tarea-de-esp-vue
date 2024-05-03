d28c5773fe8f616cf22a81676d5be0df1dcd1080
AgustinTroncoso <a.troncoso09@ufromail.cl>
2024-05-03 15:19:22 -0400

    Se agrega todo

---
+++ components/TheWelcome.vue
@@ -0,0 +1,26 @@
+<script setup>
+import{ref} from 'vue'
+const titulo='Componentecon script setup'
+const contador= ref(0)
+function incrementar(){
+  contador.value++
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <button @click="incrementar">Incrementar</button>
+    <h2>Contador:{{ contador }}</h2>
+  </div>
+</template>
+
+<style scoped>
+h1{
+  color:blue;
+}
+p{
+  color: greenz;
+}
+</style>
